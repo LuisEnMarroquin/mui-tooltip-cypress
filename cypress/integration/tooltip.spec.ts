2dc110aed132d56218d695b5cb2a075cc5dbade1
LuisEnMarroquin <mluis651@gmail.com>
2022-05-11 04:00:17 -0500

    fix: cy.injectAxe can be called only when cy.visit ends

--- cypress/integration/tooltip.spec.ts
+++ cypress/integration/tooltip.spec.ts
@@ -1,18 +1,17 @@
 describe("My First Test", () => {
   beforeEach("Visit page where is running", () => {
     cy.visit("https://axe.pages.dev");
+    cy.injectAxe();
   });
 
   it("Testing accessibility on open", () => {
     expect(true).to.equal(true);
-    cy.injectAxe();
     cy.checkA11y();
   });
 
   it("Testing tooltip accessibility", () => {
     cy.get('[data-testid="DeleteIcon"]').trigger("mouseover");
     cy.contains("DeleteText").should("be.visible");
-    cy.injectAxe();
     cy.checkA11y();
   });
 });
