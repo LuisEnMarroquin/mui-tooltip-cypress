c9792fe6394f5d6bfb9ccda111b907a93c98d573
LuisEnMarroquin <mluis651@gmail.com>
2022-05-12 12:57:20 -0500

    Testing that tooltip doesnt break a11y

--- cypress/integration/tooltip.spec.ts
+++ cypress/integration/tooltip.spec.ts
@@ -1,6 +1,7 @@
 describe("My First Test", () => {
   beforeEach("Visit page where is running", () => {
-    cy.visit("https://axe.pages.dev");
+    // cy.visit("https://axe.pages.dev");
+    cy.visit("http://localhost:3000");
     cy.injectAxe();
   });
 
@@ -9,9 +10,18 @@ describe("My First Test", () => {
     cy.checkA11y();
   });
 
-  it("Testing tooltip accessibility", () => {
+  it("Testing tooltip accessibility default", () => {
     cy.get('[data-testid="DeleteIcon"]').trigger("mouseover");
     cy.contains("DeleteText").should("be.visible");
+    cy.checkA11y();
+  });
+
+  it("Tooltip accessibility when color changes", () => {
+    cy.get("button").contains("Add Class").trigger("click");
+
+    cy.get('[data-testid="DeleteIcon"]').trigger("mouseover");
+    cy.contains("ChangeText").should("be.visible");
+
     cy.checkA11y();
   });
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,66 @@
+import { useState } from "react";
+
 import DeleteIcon from "@mui/icons-material/Delete";
-import IconButton from "@mui/material/IconButton";
-import Tooltip from "@mui/material/Tooltip";
+
+import Box from "@mui/material/Box";
 import Paper from "@mui/material/Paper";
+import Button from "@mui/material/Button";
 import Container from "@mui/material/Container";
+import IconButton from "@mui/material/IconButton";
+import Tooltip, { tooltipClasses } from "@mui/material/Tooltip";
+
+const tooptipProps = {
+  tooltip: {
+    sx: {
+      color: "#fff",
+      fontSize: "10px",
+      fontWeight: "500",
+      lineHeight: "14px",
+      fontStyle: "normal",
+      fontFamily: "Roboto",
+      // Override MUI defaults
+      backgroundColor: "#616161",
+      [`& .${tooltipClasses.arrow}`]: {
+        color: "#616161",
+      },
+    },
+  },
+};
 
 function App() {
+  const [buttonClicked, setButtonClicked] = useState(false);
+
+  const handleClick = () => {
+    setButtonClicked((current) => !current);
+  };
+
   return (
     <Container sx={{ padding: "25px", textAlign: "center" }} role="main">
       <h1>Hello from React</h1>
       <Paper sx={{ padding: "25px" }}>
-        <Tooltip title="DeleteText">
+        <Tooltip
+          title={buttonClicked ? "ChangeText" : "DeleteText"}
+          componentsProps={buttonClicked ? tooptipProps : {}}
+          PopperProps={{
+            disablePortal: true,
+          }}
+        >
           <IconButton>
             <DeleteIcon />
           </IconButton>
         </Tooltip>
+        <Box>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat velit
+          vero totam debitis facilis! Dicta dolorum iusto inventore
+          reprehenderit accusamus, perspiciatis nihil expedita eum delectus
+          corrupti rerum sit eos architecto.
+        </Box>
       </Paper>
+      <Box sx={{ padding: "25px" }}>
+        <Button variant="contained" onClick={handleClick}>
+          Add Class
+        </Button>
+      </Box>
     </Container>
   );
 }
